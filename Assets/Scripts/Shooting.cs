06fdcf38e30a9bb8d6d50dd9abbcfe5a62278731
Resvith <116066494+Resvith@users.noreply.github.com>
2024-04-14 11:55:21 +0200

    Add: creating bullet with force on mouse click

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -4,13 +4,18 @@ using UnityEngine.InputSystem.UI;
 
 public class Shooting : MonoBehaviour
 {
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private GameObject _gunEnd;
+    [SerializeField] private float _bulletForce = 10f;
+
     private InputSystemUIInputModule _inputModule;
+    private Camera _mainCamera;
+
 
     private void Start()
     {
-        // Get the InputSystemUIInputModule component
-        _inputModule = GetComponentInChildren<InputSystemUIInputModule>();
-        print(_inputModule);
+        _inputModule = GameObject.FindObjectOfType<InputSystemUIInputModule>();
+        _mainCamera = Camera.main;
 
         // Subscribe to the left click event
         _inputModule.leftClick.action.performed += OnLeftClick;
@@ -18,8 +23,20 @@ public class Shooting : MonoBehaviour
 
     private void OnLeftClick(InputAction.CallbackContext context)
     {
-        Debug.Log("Left mouse button clicked!");
+        // ----------------  TO DO : Add conditions to check if the player can shoot --------------------- 
+        Shoot();   
+    }
 
+    private void Shoot()
+    {
+        // Getting shoot direction
+        Ray ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        Vector3 targetDirection = ray.direction;
 
+        GameObject bullet = Instantiate(_bulletPrefab, _gunEnd.transform.position, _gunEnd.transform.rotation, parent: _gunEnd.transform);
+
+        // Adding force to the bullet
+        Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
+        bulletRigidbody.AddForce(targetDirection * _bulletForce, ForceMode.Impulse);
     }
 }
