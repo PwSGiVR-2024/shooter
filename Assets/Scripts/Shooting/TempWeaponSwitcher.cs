db9e68000503d947ba224909c2b4d6916cb3d1c4
Resvith <116066494+Resvith@users.noreply.github.com>
2024-05-22 10:32:11 +0200

    chore(guns): add new weapons assets
    New guns added without animation controll

---
+++ Assets/Scripts/Shooting/TempWeaponSwitcher.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+
+// This class exists only for make develop easier
+// Delete this class after implementing the eq system
+public class TempWeaponSwitcher : MonoBehaviour
+{
+    public GameObject Key1;
+    public GameObject Key2;
+    public GameObject Key3;
+
+    private void Start()
+    {
+        Key1.SetActive(false);
+        Key2.SetActive(false);
+        Key3.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Key1.SetActive(true);
+            Key2.SetActive(false);
+            Key3.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Key1.SetActive(false);
+            Key2.SetActive(true);
+            Key3.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Key1.SetActive(false);
+            Key2.SetActive(false);
+            Key3.SetActive(true);
+        }
+    }
+}
