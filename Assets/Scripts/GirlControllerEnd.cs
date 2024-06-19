dc19805870e6c17a14efa909795acf8e84a850ee
CreazyBoyxD <hzienda.5a2@gmail.com>
2024-06-19 23:34:20 +0200

    Boss fight Girl Anim

--- Assets/Scripts/GirlControllerEnd.cs
+++ Assets/Scripts/GirlControllerEnd.cs
@@ -5,10 +5,29 @@ public class GirlControllerEnd : MonoBehaviour
 {
     public Transform target;
     private NavMeshAgent agent;
+    private Animator animator;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (agent != null && animator != null)
+        {
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                animator.SetBool("Idle", false);
+                animator.SetBool("Run", true);
+            }
+            else
+            {
+                animator.SetBool("Run", false);
+                animator.SetBool("Idle", true);
+            }
+        }
     }
 
     public void MoveToTarget()
