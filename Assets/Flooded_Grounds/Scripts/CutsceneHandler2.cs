15d61e213114381b3d544988290d3cf1ca82306d
CreazyBoyxD <hzienda.5a2@gmail.com>
2024-06-19 09:41:06 +0200

    feat: CutsceneHandlers

--- Assets/Flooded_Grounds/Scripts/CutsceneHandler2.cs
+++ Assets/Flooded_Grounds/Scripts/CutsceneHandler2.cs
@@ -1,11 +1,12 @@
+using StarterAssets;
 using UnityEngine;
 using UnityEngine.Playables;
 
-public class CutScene1 : MonoBehaviour
+public class CutsceneHandler2 : MonoBehaviour
 {
-    public Transform dziad;
-    public Animator anim;
     public PlayableDirector dir;
+    public FirstPersonController fpc;
+    private bool hasCutscenePlayed = false;
 
     void Start()
     {
@@ -15,18 +16,20 @@ public class CutScene1 : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        dir.Play();
+        if (!hasCutscenePlayed && other.CompareTag("Player"))
+        {
+            dir.Play();
+            hasCutscenePlayed = true;
+        }
     }
 
     private void OnCutsceneStarted(PlayableDirector dir)
     {
-        anim.SetBool("Talking", true);
-        dziad.position = new Vector3(334.365f, 102.0025f, 173.702f);
+        fpc.enabled = false;
     }
 
     private void OnCutsceneEnded(PlayableDirector pd)
     {
-        anim.SetBool("Talking", false);
-        dziad.position = new Vector3(334.5662f, 101.975f, 173.6883f);
+        fpc.enabled = true;
     }
 }
