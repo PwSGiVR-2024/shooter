7be75aca0d8172a2bd871d5b862bee0760130f6d
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-06-24 22:28:41 +0200

    fix: inventory press again close bug, open inv on other containers

---
+++ Assets/Scripts/Inventory/ItemContainerCloser.cs
@@ -0,0 +1,41 @@
+﻿using DevionGames.InventorySystem;
+using UnityEngine;
+
+public class ItemContainerCloser : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ItemContainer container = GameObject.Find("Crafting").GetComponent<ItemContainer>();
+            if (container.IsVisible)
+            {
+                container.Close();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            GameObject shopContainerGo = GameObject.Find("Vendor");
+            if (shopContainerGo != null)
+            {
+                ItemContainer shopContainer = shopContainerGo.GetComponent<ItemContainer>();
+                if (shopContainer.IsVisible)
+                {
+                    shopContainer.Close();
+                }
+            }
+            else
+            {
+                GameObject lootContainerGo = GameObject.Find("Chest");
+                if (lootContainerGo != null)
+                {
+                    ItemContainer lootContainer = lootContainerGo.GetComponent<ItemContainer>();
+                    if (lootContainer != null && lootContainer.IsVisible)
+                    {
+                        lootContainer.Close();
+                    }
+                }
+            }
+        }
+    }
+}
