5ca0de87f3327e8c86f625b8ff7fec2aa9401f6f
Resvith <116066494+Resvith@users.noreply.github.com>
2024-06-24 16:54:48 +0200

    chore: add buged anim to melee weapon

--- Assets/Scripts/Shooting/MeleeWeapon.cs
+++ Assets/Scripts/Shooting/MeleeWeapon.cs
@@ -6,9 +6,42 @@ public class MeleeWeapon : MonoBehaviour
     private float _dmg;
     private float _range;
 
+    private bool _isAttacking = false;
+    private bool _isChainAttack = false;
+
     public Animator Animator => _animator;
     public float Dmg { get => _dmg; set => _dmg = value; }
     public float Range { get => _range; set => _range = value; }
+
+    public bool IsAttacking
+    {
+        get => _isAttacking;
+        set
+        {
+            _isAttacking = value;
+            _animator.SetBool("isAttacking", value);
+        }
+    }
+    public bool IsChainAttack
+    {
+        get => _isChainAttack;
+        set
+        {
+            _isChainAttack = value;
+            _animator.SetBool("isChainAttack", value);
+        }
+    }
+
+    public void OnAttack1End()
+    {
+        IsAttacking = false;
+    }
+
+    public void OnAttack2End()
+    {
+        IsAttacking = false;
+        IsChainAttack = false;
+    }
 
 
     private void Start()
--- Assets/Scripts/Shooting/MeleeWeaponManager.cs
+++ Assets/Scripts/Shooting/MeleeWeaponManager.cs
@@ -7,15 +7,21 @@ public class MeleeWeaponManager : MonoBehaviour
     private PlayerInput _playerInput;
     private StarterAssetsInputs _input;
     [SerializeField] private MeleeWeapon _fireaxe;
+
     void Start()
     {
         _input = GameObject.FindGameObjectWithTag("Player").GetComponent<StarterAssetsInputs>();
         _playerInput = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInput>();
+        _playerInput.actions["Fire"].started += OnAttackTap;
     }
 
-    void Update()
+    private void OnAttackTap(InputAction.CallbackContext context)
     {
-        if (_input.fire)
+        if (_fireaxe.IsAttacking)
+        {
+            _fireaxe.IsChainAttack = true;
+        }
+        else
         {
             MeleeAttack();
         }
@@ -23,6 +29,7 @@ public class MeleeWeaponManager : MonoBehaviour
 
     void MeleeAttack()
     {
+        _fireaxe.IsAttacking = true;
         _fireaxe.Animator.SetTrigger("Attack");
     }
 }
