4172815ccdece213b38ddd36154c35e80aa00d27
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-06-29 17:11:39 +0200

    feat: make rolling objects roll slower

--- Assets/Scripts/Inventory/GhoulDropSpawner.cs
+++ Assets/Scripts/Inventory/GhoulDropSpawner.cs
@@ -12,7 +12,6 @@ public class GhoulDropSpawner : MonoBehaviour
     {
         yield return new WaitForSeconds(time);
         int ghoulRemainsCount = Random.Range(0, 3);
-        Debug.Log(ghoulRemainsCount);
         for (int i = 0; i < ghoulRemainsCount; i++)
         {
             Vector3 offset = new Vector3(Random.Range(-1 * _offset, _offset), _offset, Random.Range(-1 * _offset, _offset));
