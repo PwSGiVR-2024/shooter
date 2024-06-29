a7a99369a697ccccd167ba08d7769bd757635a0d
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-06-29 16:10:00 +0200

    fix: drop items

---
+++ Assets/Scripts/Inventory/GhoulDropSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GhoulDropSpawner : MonoBehaviour
+{
+    public void Spawn(float delay)
+    {
+        StartCoroutine(DelayDrop(delay));
+    }
+    IEnumerator DelayDrop(float time)
+    {
+        yield return new WaitForSeconds(time);
+        int ghoulRemainsCount = Random.Range(0, 3);
+        for (int i = 0; i < ghoulRemainsCount; i++)
+        {
+            Vector3 offset = new Vector3(Random.Range(-0.5f, 0.5f), 0, Random.Range(-0.5f, 0.5f));
+            Instantiate(GameObject.Find("ItemPrefabs").GetComponent<ItemPrefabs>().GhoulRemains, transform.position + offset, transform.rotation);
+        }
+
+        int rottenApplesCount = Random.Range(0, 4);
+        for (int i = 0; i < rottenApplesCount; i++)
+        {
+            Vector3 offset = new Vector3(Random.Range(-0.5f, 0.5f), 0, Random.Range(-0.5f, 0.5f));
+            Instantiate(GameObject.Find("ItemPrefabs").GetComponent<ItemPrefabs>().RottenApple, transform.position + offset, transform.rotation);
+        }
+    }
+}
