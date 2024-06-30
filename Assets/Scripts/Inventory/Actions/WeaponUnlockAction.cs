c7fe72dfa5251a5ebe1927b1b6de6494c01dad57
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-06-30 04:43:56 +0200

    feat: weapons as items

---
+++ Assets/Scripts/Inventory/Actions/WeaponUnlockAction.cs
@@ -0,0 +1,15 @@
+﻿using DevionGames;
+using UnityEngine;
+
+[ComponentMenu("Custom/Weapon Unlock Action")]
+[System.Serializable]
+public class WeaponUnlock : Action
+{
+    [SerializeField] string _weaponName;
+
+    public override ActionStatus OnUpdate()
+    {
+        WeaponManager.Instance.UnlockWeapon(_weaponName);
+        return ActionStatus.Success;
+    }
+}
